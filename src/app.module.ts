ccf2bbb1dc793b8c8abb621bb0733906631387b5
arlesson menezes <arlesson.a7x@gmail.com>
2023-06-03 23:59:08 -0300

    feat/create connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.DB_LOCALHOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true
+    }as TypeOrmModuleOptions),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
